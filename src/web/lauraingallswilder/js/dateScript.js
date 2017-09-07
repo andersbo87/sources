f8bc293a6941ead89adf527938934346f383d09d
Anders <andersbo87@icloud.com>
2017-09-07 15:00:45 +0200

    Fjernet gamle og unødige filer og mapper.

--- src/web/lauraingallswilder/js/dateScript.js
+++
@@ -1,5 +0,0 @@
-var currentTime = new Date();
-var day = currentTime.getDate();
-var month = currentTime.getMonth() + 1;
-var year = currentTime.getFullYear();
-document.write(day + "." + month + "." + year);
