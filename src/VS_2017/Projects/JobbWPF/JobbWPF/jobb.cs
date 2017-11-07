624ad507b500da7c56b752c1ec240bdc8c0c5665
Anders Bolt-Evensen <andersbo87@icloud.com>
2017-11-07 15:12:58 +0100

    En klasse som brukes med dataGrid i filen ViewSpecificApplications.cs og tilhørende klasse

---
+++ src/VS_2017/Projects/JobbWPF/JobbWPF/jobb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobbWPF
+{
+    // For å kunne fylle min dataGrid på en enkel måte, oppretter jeg en egen klasse kalt jobb som inneholder heltall og strenger
+    // for søknadid, tittel, bedrift, stedsnavn, status og søknadsfrist.
+    class jobb
+    {
+        public int applicationID { get; set; }
+        public string jobTitle { get; set; }
+        public string company { get; set; }
+        public string cityName { get; set; }
+        public string statusName { get; set; }
+        public string deadline { get; set; }
+    }
+}
