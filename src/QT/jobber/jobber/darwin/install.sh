e745dd7cd2e597a40b50a3d127eeb7ce3fd4dca3
Anders Bolt-Evensen <andersbo87@icloud.com>
2017-11-27 19:51:22 +0100

    Oppdatert README-fila.

--- src/QT/jobber/jobber/darwin/install.sh
+++ src/QT/jobber/jobber/darwin/install.sh
@@ -3,11 +3,20 @@
 if [ -e "/opt/local/Qt/5.9.2/clang_64/bin/qmake" ]
 then
     /opt/local/Qt/5.9.2/clang_64/bin/qmake -o Makefile ../jobber.pro -spec macx-clang
+else
+    echo "Please install QT5 by typing `port install qt5.x`, e.g. qt5.9."
+    exit 1
+fi
+
+if [ "$1" == "uninstall" ]
+then
+    make uninstall clean
+    rm -rf /Applications/jobber.app/
+    rm Makefile
+else
     /usr/bin/make
     /bin/rm -f install
     /bin/cp -R jobber.app/ /Applications/jobber.app/
     /usr/bin/make clean
-else
-    echo "Please install QT5 by typing `port install qt5.x`, e.g. qt5.9."
-    exit 1
+    rm -rf jobber.app/ Makefile
 fi
