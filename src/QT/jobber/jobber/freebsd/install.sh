b115258a66fc5dd9728b602d4baeb1ad19deb363
Anders <andersbo87@icloud.com>
2017-11-27 19:33:29 +0100

    Små endringer i freebsd/install.sh.

--- src/QT/jobber/jobber/freebsd/install.sh
+++ src/QT/jobber/jobber/freebsd/install.sh
@@ -7,10 +7,18 @@ fi
 
 if [ -e /usr/local/lib/qt5/bin/qmake ]
 then
-    /usr/local/lib/qt5/bin/qmake -spec freebsd-clang -o Makefile ../jobber.pro
-    make install clean
-    rm -f install
+    /usr/local/lib/qt5/bin/qmake -spec freebsd-clang -o ../Makefile ../jobber.pro
 else
     echo "Please install all of QT5 using `pkg install qt5` or cd `/usr/ports/devel/qt5/ && make install clean`."
     exit 1
+fi
+
+if [ "$1" != "uninstall" ]
+then
+    make -C ../ install clean
+    rm -f install
+    rm ../Makefile
+else
+    make -C ../ uninstall clean
+    rm ../Makefile
 fi
