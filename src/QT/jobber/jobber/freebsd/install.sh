f8e22baa3fe88b1dc8eff94e33ccec6c5a53936c
Anders <andersbo87@icloud.com>
2017-12-04 16:00:37 +0100

    Små endringer i install.sh for å kunne kompilere programmet uten å måtte 'installere' det først.

--- src/QT/jobber/jobber/freebsd/install.sh
+++ src/QT/jobber/jobber/freebsd/install.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-if [ $(id -u) != 0 ]
-then
-    echo "This script must be run as root."
-    exit 1
-fi
+
+checkroot(){
+    # Sjekke om brukeren er rotbruker eller ikke.
+    if [ $(id -u) != 0 ]
+    then
+	echo "This script must be run as root."
+	exit 1
+    fi
+}
 
 if [ -e /usr/local/lib/qt5/bin/qmake ]
 then
@@ -12,13 +16,22 @@ else
     echo "Please install all of QT5 using `pkg install qt5` or cd `/usr/ports/devel/qt5/ && make install clean`."
     exit 1
 fi
-
-if [ "$1" != "uninstall" ]
+if [ "$1" == "uninstall" ]
 then
-    make -C ../ install clean
-    rm -f install
+    checkroot
+    make -C ../ uninstall clean
+    rm ../Makefile
+elif [ "$1" == "compile" ]
+then
+    make -C ../
+    rm ../Makefile
+elif [ "$1" == "clean" ]
+then
+    make -C ../ clean
     rm ../Makefile
 else
-    make -C ../ uninstall clean
+    checkroot
+    make -C ../ install clean
+    rm -f install
     rm ../Makefile
 fi
