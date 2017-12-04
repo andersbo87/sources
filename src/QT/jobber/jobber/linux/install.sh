322c13846869e7900a872a4b7649d37d07f14c8d
Anders <andersbo87@icloud.com>
2017-12-04 16:07:11 +0100

    Små endringer i install.sh for å kunne kompilere programmet uten å måtte 'installere' det først.

--- src/QT/jobber/jobber/linux/install.sh
+++ src/QT/jobber/jobber/linux/install.sh
@@ -1,17 +1,34 @@
 #!/bin/sh
-
-if [ $(id -u) != 0 ]
-then
-    echo "This script must be run as root."
-    exit 1
-fi
-
+checkroot(){
+    if [ $(id -u) != 0 ]
+    then
+	echo "This script must be run as root."
+	exit 1
+    fi
+}
 if [ -e "/usr/lib/x86_64-linux-gnu/qt5/bin/qmake" ]
 then
     /usr/lib/x86_64-linux-gnu/qt5/bin/qmake -o ../Makefile ../jobber.pro -spec linux-clang
+else
+    echo "Please install QT5 before running this script."
+    exit 1
+fi
+
+if [ "$1" == "uninstall" ]
+then
+    checkroot
+    make -C ../ uninstall clean
+    rm ../Makefile
+elif [ "$1" == "compile" ]
+then
+    make -C ../
+    rm ../Makefile
+elif [ "$1" == "clean" ]
+then
+    make -C ../ clean
+    rm ../Makefile
+else
     make -C ../
     make -C ../ install clean
     rm -f install ../Makefile
-else
-    echo "Please install QT5 before running this script."
 fi
