1facf8bd9aeda98f7b7ad1c0b9c4f4bcee95e16a
Anders Bolt-Evensen <andersbo87@icloud.com>
2018-02-28 12:53:52 +0100

    Fjernet GUID fra Arrays\AssemblyInfo.cs.

--- repos/HelloWorld/Arrays/Properties/AssemblyInfo.cs
+++ repos/HelloWorld/Arrays/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("459e5381-f871-42b6-9346-aec51bbca632")]
+//[assembly: Guid("459e5381-f871-42b6-9346-aec51bbca632")]
 
 // Version information for an assembly consists of the following four values:
 //
