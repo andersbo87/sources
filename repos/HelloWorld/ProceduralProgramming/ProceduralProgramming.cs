1df231204bb6bd0457786dcaa55ccd74688cbd53
Anders Bolt-Evensen <andersbo87@icloud.com>
2018-02-28 11:58:14 +0100

    La til monodevelop ProceduralProgramming.

--- repos/HelloWorld/ProceduralProgramming/ProceduralProgramming.cs
+++ repos/HelloWorld/ProceduralProgramming/ProceduralProgramming.cs
@@ -4,9 +4,22 @@ namespace ProceduralProgramming
 {
 	class ProceduralProgramming
 	{
-		public static void Main(string[] args)
+		/*public static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
+			Console.Write("Hva heter du? ");
+			string res = Console.ReadLine();
+			Console.WriteLine("I reversert rekkefølge blir navnet ditt " + reverseUsername(res));
+		}*/
+
+		static string reverseUsername(string name)
+		{
+			char[] array = new char[name.Length];
+			for (int i = name.Length; i > 0; i--)
+			{
+				array[name.Length - i] = name[i - 1];
+			}
+			return new string(array);
 		}
 	}
 }
---
+++ repos/HelloWorld/ProceduralProgramming/ProceduralProgramming2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ProceduralProgramming
+{
+	public class ProceduralProgramming2
+	{
+		static void Main(string[] args)
+		{
+			List<int> liste = new List<int>();
+			while (true)
+			{
+				Console.Write("Vennligst skriv inn et tall: ");
+				string ans = Console.ReadLine().ToLower();
+				if (string.Compare(ans, "quit") == 0)
+				{
+					break;
+				}
+				liste.Add(int.Parse(ans));
+			}
+			Console.WriteLine("Unike numre:");
+			foreach (int i in getUniqueNumbers(liste))
+				Console.Write(i + " ");
+			Console.WriteLine("");
+		}
+
+		static List<int> getUniqueNumbers(List<int> numbers)
+		{
+			List<int> uniques = new List<int>();
+			foreach (int i in numbers)
+			{
+				if(!uniques.Contains(i))
+					uniques.Add(i);
+			}
+			return uniques;
+		}
+	}
+}
