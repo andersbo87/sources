0cda9ef08861bf5f33263ba783d40301230446a7
Anders Bolt-Evensen <andersbo87@icloud.com>
2019-12-11 21:31:39 +0100

    Forbedret recordstream.

--- src/sh/recordstream/recordstream.sh
+++ src/sh/recordstream/recordstream.sh
@@ -15,8 +15,12 @@ sl_exists=`which streamlink`
 #    exit 1
 #fi
 
+echo $#
+
 slink()
 {
+    echo "Parameters: $@"
+    echo "Parameter1: $1"
     if [ "$#" -lt 2 ]
     then
 	echo "Please provide at least username and video quality."
@@ -30,20 +34,42 @@ slink()
 	    echo "Please install VLC."
 	    exit 1
 	else
-	    streamlink --player "/usr/local/bin/vlc --file-caching 1000 --network-caching 4000" https://www.twitch.tv/$1 $2
+	    for substr in "twitch"; do
+		if [ -z "${1##*$substr*}" ]
+		then
+		    #streamlink --player "/usr/local/bin/vlc --file-caching 1000 --network-caching 4000" $1 $2
+		    streamlink --player "$vlc_exists --file-caching 1000 --network-caching 4000" $1 $2
+		else
+		    streamlink --player "$vlc_exists --file-caching 1000 --network-caching 4000" https://www.twitch.tv/$1 $2
+		fi
+	    done
 	fi
     elif [ "$#" -eq "3" ]
     then
 	ext=`echo ${3} | cut -d"." -f2`
 	filename=`echo ${3} | cut -d"." -f1`
-	if [ -e "$3" ]
-	then
-	    echo "executing livestreamer https://www.twitch.tv/$1 $2"
-	    echo "$3 already exits. Writing to $filename-$(date +%d.%m.%Y_%H.%M.%S).$ext instead."
-	    streamlink https://www.twitch.tv/$1 $2 -o "$filename-$(date +%d.%m.%Y_%H.%M.%S).$ext"
-	else
-	    streamlink https://www.twitch.tv/$1 $2 -o $3
-	fi
+	for substr in "twitch"; do
+	    if [ -z "${1##*$substr*}" ]
+	    then
+		if [ -e "$3" ]
+		then
+		    echo "executing livestreamer $1 $2"
+		    echo "$3 already exits. Writing to $filename-$(date +%d.%m.%Y_%H.%M.%S).$ext instead."
+		    streamlink $1 $2 -o "$filename-$(date +%d.%m.%Y_%H.%M.%S).$ext"
+		else
+		    streamlink $1 $2 -o $3
+		fi
+	    else
+		if [ -e "$3" ]
+		then
+		    echo "executing livestreamer https://www.twitch.tv/$1 $2"
+		    echo "$3 already exits. Writing to $filename-$(date +%d.%m.%Y_%H.%M.%S).$ext instead."
+		    streamlink https://www.twitch.tv/$1 $2 -o "$filename-$(date +%d.%m.%Y_%H.%M.%S).$ext"
+		else
+		    streamlink https://www.twitch.tv/$1 $2 -o $3
+		fi
+	    fi
+	done
     elif [ "$#" -gt "3" ]
     then
 	echo "$0 does not accept more than 3 arguments."
@@ -65,20 +91,41 @@ lstream() {
 	    echo "Please install VLC."
 	    exit 1
 	else
-	    livestreamer --player "/usr/local/bin/vlc --file-caching 1000 --network-caching 4000" https://www.twitch.tv/$1 $2
+	    for substr in "twitch"; do
+		if [ -z "${1##*$substr*}" ]
+		then
+		    livestreamer --player "$vlc_exists --file-caching 1000 --network-caching 4000" $1 $2
+		else
+		    livestreamer --player "$vlc_exists --file-caching 1000 --network-caching 4000" https://www.twitch.tv/$1 $2
+		fi
+	    done
 	fi
     elif [ "$#" -eq "3" ]
     then
 	ext=`echo ${3} | cut -d"." -f2`
 	filename=`echo ${3} | cut -d"." -f1`
-	if [ -e "$3" ]
-	then
-	    echo "executing livestreamer https://www.twitch.tv/$1 $2"
-	    echo "$3 already exits. Writing to $filename-$(date +%d.%m.%Y_%H.%M.%S).$ext instead."
-	    livestreamer https://www.twitch.tv/$1 $2 -o "$filename-$(date +%d.%m.%Y_%H.%M.%S).$ext"
-	else
-	    livestreamer https://www.twitch.tv/$1 $2 -o $3
-	fi
+	for substr in "twitch"; do
+	    if [ -z "${1##*$substr*}" ]
+	    then
+		if [ -e "$3" ]
+		then
+		    echo "executing livestreamer $1 $2"
+		    echo "$3 already exits. Writing to $filename-$(date +%d.%m.%Y_%H.%M.%S).$ext instead."
+		    livestreamer $1 $2 -o "$filename-$(date +%d.%m.%Y_%H.%M.%S).$ext"
+		else
+		    livestreamer $1 $2 -o $3
+		fi
+	    else
+		if [ -e "$3" ]
+		then
+		    echo "executing livestreamer https://www.twitch.tv/$1 $2"
+		    echo "$3 already exits. Writing to $filename-$(date +%d.%m.%Y_%H.%M.%S).$ext instead."
+		    livestreamer https://www.twitch.tv/$1 $2 -o "$filename-$(date +%d.%m.%Y_%H.%M.%S).$ext"
+		else
+		    livestreamer https://www.twitch.tv/$1 $2 -o $3
+		fi
+	    fi
+	done
     elif [ "$#" -gt "3" ]
     then
 	echo "$0 does not accept more than 3 arguments."
@@ -93,25 +140,29 @@ askuser()
     while read ans; do
 	if [ "$ans" = "ls" ]
 	then
-	    lstream
+	    lstream $1 $2 $3
 	elif [ "$ans" = "sl" ]
 	then
-	    slink
+	    slink $1 $2 $3
 	else
 	    askuser
 	fi
+	exit 0
     done
 }    
 
 if [ "$ls_exists" != "" -a "$sl_exists" == "" ]
 then
     lstream
+    exit 0
 elif [ "$sl_exists" != "" -a "$ls_exists" = "" ]
 then
     slink
+    exit 0
 elif [ "$ls_exists" != "" -a "$sl_exists" != "" ]
 then
-    askuser
+    askuser $1 $2 $3
+    exit 0
 else
     echo "Please install either livestreamer or streamlink"
     exit 1
